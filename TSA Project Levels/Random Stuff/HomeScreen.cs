2efa82bdb9986a22d6bfc87caada5c4480755a36
srschander06 <84356517+srschander06@users.noreply.github.com>
2024-09-16 17:20:36 -0400

    Add files via upload
    these files of code are for the user settings of the game

---
+++ TSA Project Levels/Random Stuff/HomeScreen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class HomeScreen : MonoBehaviour
+{
+    public void GoHome()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+}
